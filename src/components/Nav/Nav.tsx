72029347dd075d80a18e936e77c5baccf2eac944
Sarah Veness <sarahhveness@gmail.com>
2023-06-22 14:50:16 -0700

    set up Nav component

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './Nav.module.scss'
+
+export default function Nav() {
+  return (
+    <div className={styles.Nav}>
+      <div className={styles.Links}>
+        <a href="/about">SARAH</a>
+        <a href="/read">READING</a>
+        <a href='/uses'>USES</a>
+        <a href="/contact">CONTACT</a>
+      </div>
+    </div>
+  );
+}
