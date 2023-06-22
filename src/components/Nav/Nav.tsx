a2d743b6b8a09a25053ccd3a778678135795f6e7
Sarah Veness <sarahhveness@gmail.com>
2023-06-22 15:57:27 -0700

    change the apostrophes ergh

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,10 +6,10 @@ export default function Nav() {
   return (
     <div className={styles.Nav}>
       <div className={styles.Links}>
-        <a href="/about">SARAH</a>
-        <a href="/read">READING</a>
+        <a href='/about'>SARAH</a>
+        <a href='/read'>READING</a>
         <a href='/uses'>USES</a>
-        <a href="/contact">CONTACT</a>
+        <a href='/contact'>CONTACT</a>
       </div>
     </div>
   );
