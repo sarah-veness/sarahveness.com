d435a2d9163f298c946fb9bd0c3885b091615faa
Sarah Veness <sarahhveness@gmail.com>
2023-06-29 14:54:21 -0700

    get overlay for text descriptions cleaned up

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,6 +6,11 @@ export default function Card() {
   return (
     <div className={styles.Card}>
       <img src='https://placekitten.com/g/500/500' />
+      <div className={styles.Overlay}>
+        <h2>Card Title</h2>
+        <p>Card Description</p>
+        <button>-&gt;</button>
+      </div>
     </div>
   )
 }
