a178e20dfab61db112c96d28ab9468ba62072eb4
Sarah Veness <sarahhveness@gmail.com>
2023-06-29 16:26:21 -0700

    make the whole card clickable

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,12 +5,15 @@ import styles from './Card.module.scss'
 export default function Card() {
   return (
     <div className={styles.Card}>
-      <img src='https://placekitten.com/g/500/500' />
-      <div className={styles.Overlay}>
-        <h2>Card Title</h2>
-        <p>Card Description</p>
-        <button>-&gt;</button>
-      </div>
+      <a href="google.com">
+        <img src='https://placekitten.com/g/500/500' />
+        <div className={styles.Overlay}>
+          <div className={styles.textContainer}>
+            <h2>Card Title</h2>
+            <p>Card Description</p>
+          </div>
+        </div>
+      </a>
     </div>
   )
 }
