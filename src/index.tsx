7b79c612b0481c5852c0de92334dffe64af4a863
Sarah Veness <sarahhveness@gmail.com>
2023-08-08 13:03:25 -0700

    add auth0 package and configure client id + callback url

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Auth0Provider } from '@auth0/auth0-react'
 import reportWebVitals from './reportWebVitals';
+
+import App from './App';
+import './index.css';
+
+console.log(import.meta.env.VITE_AUTH0_CLIENT_ID)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode >
+  <Auth0Provider
+    domain="dashboard-pro-eu-au.auth0.com"
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: import.meta.env.VITE_AUTH_REDIRECT_URI,
+    }}
+  >
+
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode >
+  </Auth0Provider >
 );
 
 // If you want to start measuring performance in your app, pass a function
