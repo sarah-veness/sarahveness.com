5441b4ce91f258c193b386fcbc4cf9a2a365bdd1
Sarah Veness <sarahhveness@gmail.com>
2023-08-09 01:21:15 -0700

    create dummy NewPost component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,13 @@ import Card from './Card/Card';
 import CardContainer from './CardContainer/CardContainer';
 import AuthButtons from './AuthButtons/AuthButtons';
 import Dashboard from './Dashboard/Dashboard';
+import NewPost from './NewPost/NewPost';
 
 export {
   Nav,
   Card,
   CardContainer,
   AuthButtons,
-  Dashboard
+  Dashboard,
+  NewPost
 };
