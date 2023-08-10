20dba7ff9183e109fd73708284c861cc50e7d184
Sarah Veness <sarahhveness@gmail.com>
2023-08-10 14:29:00 -0700

    make ShowPost only responsible for showing

--- src/components/ShowPost/ShowPost.tsx
+++ src/components/ShowPost/ShowPost.tsx
@@ -1,64 +1,51 @@
 import { useState, useEffect } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 import { Post } from '../../types/Post';
 
 export default function ShowPost() {
-  const [post, setPost] = useState({} as Post)
+  const [post, setPost] = useState({} as Post);
 
-  const navigate = useNavigate();
   const { id } = useParams();
-
 
   useEffect(() => {
     axios
       .get(`http://localhost:3000/api/posts/${id}`)
       .then((res) => {
-        setPost(res.data)
+        setPost(res.data);
       })
-      .catch(err => {
-        console.error(`Error in ShowPost: ${err.message}`)
-      })
-  }, [id])
-
-  const onDeleteClick = (id: any) => {
-    axios
-      .delete(`http://localhost:3000/api/posts/${id}`)
-      .then((res) => {
-        navigate('/')
-      })
-      .catch(err => {
-        console.error(`Error in ShowPostDeleteClick: ${err.message}`)
-      })
-  };
+      .catch((err) => {
+        console.error(`Error in ShowPost: ${err.message}`);
+      });
+  }, [id]);
 
   const Post = (
     <div>
-      <table className='table table-hover table-dark'>
+      <table className="table table-hover table-dark">
         <tbody>
           <tr>
-            <th scope='row'>1</th>
+            <th scope="row">1</th>
             <td>Title</td>
             <td>{post.title}</td>
           </tr>
           <tr>
-            <th scope='row'>2</th>
+            <th scope="row">2</th>
             <td>Author</td>
             <td>{post.author}</td>
           </tr>
           <tr>
-            <th scope='row'>4</th>
+            <th scope="row">4</th>
             <td>Publisher</td>
             <td>{post.publisher}</td>
           </tr>
           <tr>
-            <th scope='row'>5</th>
+            <th scope="row">5</th>
             <td>Published Date</td>
             <td>{post.published_date}</td>
           </tr>
           <tr>
-            <th scope='row'>6</th>
+            <th scope="row">6</th>
             <td>Description</td>
             <td>{post.content}</td>
           </tr>
@@ -68,43 +55,19 @@ export default function ShowPost() {
   );
 
   return (
-    <div className='ShowpostDetails'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-10 m-auto'>
+    <div className="ShowpostDetails">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-10 m-auto">
             <br /> <br />
-            <Link to='/' className='btn btn-outline-warning float-left'>
-              Show post List
+            <Link to="/" className="btn btn-outline-warning float-left">
+              Back to Posts
             </Link>
           </div>
-          <br />
-          <div className='col-md-8 m-auto'>
-            <h1 className='display-4 text-center'>post's Record</h1>
-            <p className='lead text-center'>View post's Info</p>
-            <hr /> <br />
-          </div>
-          <div className='col-md-10 m-auto'>{Post}</div>
-          <div className='col-md-6 m-auto'>
-            <button
-              type='button'
-              className='btn btn-outline-danger btn-lg btn-block'
-              onClick={() => {
-                onDeleteClick(id);
-              }}
-            >
-              Delete post
-            </button>
-          </div>
-          <div className='col-md-6 m-auto'>
-            <Link
-              to={`/admin/edit-post/${id}`}
-              className='btn btn-outline-info btn-lg btn-block'
-            >
-              Edit post
-            </Link>
-          </div>
+          <hr />
+          <div className="col-md-10 m-auto">{Post}</div>
         </div>
       </div>
     </div>
   );
-};
+}
