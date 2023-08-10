d882658828dcb60d4bc5a4f0781516cba7dafa15
Sarah Veness <sarahhveness@gmail.com>
2023-08-10 14:10:08 -0700

    skeleton add posts to dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import usePosts from "../../hooks/usePosts";
+
+import type { Post } from "../../types/Post";
+
+import styles from "./dashboard.module.scss";
+
 export default function Dashboard() {
+  let posts = usePosts();
+
+  const postList =
+    posts.length === 0
+      ? "There are no posts yet!"
+      : posts.map((post: Post, k: number) => {
+          return (
+            <Link to={`/admin/edit-post/${post._id}`}>
+              <li key={k}>{post.title}</li>
+            </Link>
+          );
+        });
+
   return (
-    <div>
+    <div className={styles.Dashboard}>
       <h1>Dashboard</h1>
+      <ul>{postList}</ul>
     </div>
-  )
+  );
 }
