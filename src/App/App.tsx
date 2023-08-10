4f81ddbd7a62a5151b4208a9d8c96a8bbaf0fc6e
Sarah Veness <sarahhveness@gmail.com>
2023-08-09 17:25:39 -0700

    set up layouts for public vs admin pages

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import { Nav, CardContainer, AuthButtons, Dashboard, CreatePost, Contact, Uses, UpdatePost, ShowPost } from '../components';
+import { CardContainer, AuthButtons, Dashboard, CreatePost, Contact, Uses, UpdatePost, ShowPost } from '../components';
+import { AdminLayout, PublicLayout } from '../layout';
 
 import { ProtectedRoute } from '../routes'
 
@@ -7,31 +8,33 @@ function App() {
 
   return (
     <>
-      <Nav />
       <Routes>
-        <Route path="/" element={<CardContainer />} />
-        <Route path="/reading" element={<CardContainer />} />
-        <Route path="/uses" element={<Uses />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<CardContainer />} />
+          <Route path="/reading" element={<CardContainer />} />
+          <Route path="/uses" element={<Uses />} />
+          <Route path="/contact" element={<Contact />} />
 
-        <Route path="/writing" element={<CardContainer />} />
-        <Route path="/writing/:id" element={<ShowPost />} />
+          <Route path="/writing" element={<CardContainer />} />
+          <Route path="/writing/:id" element={<ShowPost />} />
+        </Route>
 
         <Route path="/auth" element={<AuthButtons />} />
 
-        <Route
-          path='/admin'
-          element={<ProtectedRoute component={Dashboard} />}
-        />
-        <Route
-          path='/admin/new-post'
-          element={<ProtectedRoute component={CreatePost} />}
-        />
-        <Route
-          path='/admin/edit-post/:id'
-          element={<ProtectedRoute component={UpdatePost} />}
-        />
-
+        <Route element={<AdminLayout />}>
+          <Route
+            path='/admin'
+            element={<ProtectedRoute component={Dashboard} />}
+          />
+          <Route
+            path='/admin/new-post'
+            element={<ProtectedRoute component={CreatePost} />}
+          />
+          <Route
+            path='/admin/edit-post/:id'
+            element={<ProtectedRoute component={UpdatePost} />}
+          />
+        </Route>
       </Routes>
     </>
   )
---
+++ src/layout/index.ts
@@ -0,0 +1,7 @@
+import PublicLayout from './PublicLayout/PublicLayout';
+import AdminLayout from './AdminLayout/AdminLayout';
+
+export {
+  PublicLayout,
+  AdminLayout
+}
