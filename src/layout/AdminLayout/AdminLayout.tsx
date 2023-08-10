6e2f3f9805ba1912c5c98999c3a6dbd44e7c91f1
Sarah Veness <sarahhveness@gmail.com>
2023-08-10 13:49:12 -0700

    add some basic styling to admin layout

--- src/layout/AdminLayout/AdminLayout.tsx
+++ src/layout/AdminLayout/AdminLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
+import styles from './admin-layout.module.scss'
+
 export default function AdminLayout() {
   return (
-    <>
+    <div className={styles.Container}>
       <Outlet />
-    </>
+    </div>
   )
 }
