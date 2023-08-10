2273046ae76ea14ff6640948580ee689d4ebd9b6
Sarah Veness <sarahhveness@gmail.com>
2023-08-10 12:35:05 -0700

    move getPosts into custom hook for reusability

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+import type { Post } from '../types/Post'
+
+const usePosts = () => {
+  const [posts, setPosts] = useState([] as Post[]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/api/posts')
+      .then(res => {
+        setPosts(res.data);
+      })
+      .catch(err => {
+        console.error(`Error fetching posts: ${err.message}`);
+      });
+  }, []);
+
+  return posts;
+};
+
+export default usePosts;
