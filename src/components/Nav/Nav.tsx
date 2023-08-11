f8afc29036cc7f9ae9f43c22e5752f3216954a98
Sarah Veness <sarahhveness@gmail.com>
2023-08-11 16:12:42 -0700

    make the public nav sticky

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,16 +7,18 @@ import AdminNav from './AdminNav/AdminNav';
 export default function Nav() {
   const { isAuthenticated } = useAuth0();
   return (
-    <div className={styles.Nav}>
-      <div>
-        <Link to="/">SV</Link>
+    <div className={styles.fixedNav}>
+      <div className={styles.Nav}>
+        <div>
+          <Link to="/">SV</Link>
+        </div>
+        <div className={styles.mainLinks}>
+          <Link to="/writing">WRITING</Link>
+          <Link to="/reading">READING</Link>
+          <Link to="/contact">CONTACT</Link>
+        </div>
+        {isAuthenticated ? <AdminNav /> : <div></div>}
       </div>
-      <div className={styles.mainLinks}>
-        <Link to="/writing">WRITING</Link>
-        <Link to="/reading">READING</Link>
-        <Link to="/contact">CONTACT</Link>
-      </div>
-      {isAuthenticated ? <AdminNav /> : <div></div>}
     </div>
   );
 }
