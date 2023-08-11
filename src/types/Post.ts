230bc91e4386f9dabf0fd7ff98f31ce88219f56b
Sarah Veness <sarahhveness@gmail.com>
2023-08-10 21:40:13 -0700

    clean up Post type to add slug/updated_date and remove publisher

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,8 +1,9 @@
 export type Post = {
   _id: number;
   title: string;
+  slug: string;
   author: string;
   content: string;
-  publisher: string;
   published_date: string;
+  updated_date?: string;
 };
