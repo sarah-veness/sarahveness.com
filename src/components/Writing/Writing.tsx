f7d35fc4d0dfde05aac8a2ec88e0239a91c86f94
Sarah Veness <sarahhveness@gmail.com>
2023-08-10 22:45:59 -0700

    clean up writing page

--- src/components/Writing/Writing.tsx
+++ src/components/Writing/Writing.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 import Card from './Card/Card';
 import styles from './card-container.module.scss';
 
@@ -15,30 +13,5 @@ export default function Writing() {
           return <Card key={k} post={post} />;
         });
 
-  return (
-    <div className="ShowBookList">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <br />
-            <h2 className="display-4 text-center">Books List</h2>
-          </div>
-
-          <div className="col-md-11">
-            <Link
-              to="/admin/new-post"
-              className="btn btn-outline-warning float-right"
-            >
-              + Add New Book
-            </Link>
-            <br />
-            <br />
-            <hr />
-          </div>
-        </div>
-
-        <div className={styles.CardContainer}>{postList}</div>
-      </div>
-    </div>
-  );
+  return <div className={styles.CardContainer}>{postList}</div>;
 }
