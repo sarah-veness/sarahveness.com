1e2be0e4a295dea8d5f236655a89550320229c25
Sarah Veness <sarahhveness@gmail.com>
2023-08-10 20:29:48 -0700

    add hook for formatting date

--- src/components/Writing/Card/Card.tsx
+++ src/components/Writing/Card/Card.tsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
 
+import useFormattedDate from '../../../hooks/useFormattedDate';
+
 import styles from './Card.module.scss';
 
 export default function Card(props: any) {
   const post = props.post;
+  const formattedDate = useFormattedDate(props.post.published_date);
   return (
     <div className={styles.Card}>
       <Link to={`/post/${post._id}`}>
@@ -15,7 +18,7 @@ export default function Card(props: any) {
           <div className={styles.textContainer}>
             <h2>{post.title}</h2>
             <h3>{post.author}</h3>
-            <div>{post.content}</div>
+            <h3>{formattedDate}</h3>
           </div>
         </div>
       </Link>
---
+++ src/hooks/useFormattedDate.ts
@@ -0,0 +1,16 @@
+const useFormattedDate = (inputDateStr: string): string => {
+  const inputDate = new Date(inputDateStr);
+
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  const formattedDate: string = inputDate.toLocaleDateString('en-US', options);
+
+  return formattedDate;
+};
+
+export default useFormattedDate;
