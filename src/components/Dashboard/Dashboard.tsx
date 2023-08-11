357c18294dbfc0954bcabe7590b9015ce2e41750
Sarah Veness <sarahhveness@gmail.com>
2023-08-10 17:00:51 -0700

    first pass quick styling for dashboard of posts

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,65 +1,59 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import usePosts from '../../hooks/usePosts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
 
 import type { Post } from '../../types/Post';
+
+import usePosts from '../../hooks/usePosts';
 
 import styles from './dashboard.module.scss';
 
 export default function Dashboard() {
   let posts = usePosts();
 
-  const { id } = useParams();
-  const navigate = useNavigate();
-
   const onDeleteClick = (id: any) => {
     axios
       .delete(`http://localhost:3000/api/posts/${id}`)
       .then((res) => {
-        navigate('/');
+        console.log('deteled!');
       })
       .catch((err) => {
         console.error(`Error in ShowPostDeleteClick: ${err.message}`);
       });
   };
 
-  const Post = (props: any) => {
-    const post = props.post;
-    const index = props.k;
+  function PostItem({ post }: { post: Post }) {
     return (
       <li className={styles.Post}>
-        <Link to={`/admin/edit-post/${post._id}`} key={index}>
+        <Link to={`/admin/edit-post/${post._id}`} className={styles.title}>
           {post.title}
         </Link>
-        <div className="col-md-6 m-auto">
-          <button
-            type="button"
-            className="btn btn-outline-danger btn-lg btn-block"
-            onClick={() => {
-              onDeleteClick(id);
-            }}
-          >
-            Delete post
-          </button>
-        </div>
-
-        <div className="col-md-6 m-auto">
+        <div className={styles.buttonGroup}>
           <Link
             to={`/admin/edit-post/${post._id}`}
-            className="btn btn-outline-info btn-lg btn-block"
+            className={styles.editButton}
           >
-            Edit post
+            <FontAwesomeIcon icon={faPen} />
           </Link>
+          <span
+            className={styles.deleteButton}
+            onClick={() => {
+              onDeleteClick(post._id);
+            }}
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </span>
         </div>
       </li>
     );
-  };
+  }
 
   const postList =
     posts.length === 0
       ? 'There are no posts yet!'
-      : posts.map((post: Post, k: number) => {
-          return <Post post={post} k={k} />;
+      : posts.map((post, key) => {
+          return <PostItem post={post} key={key} />;
         });
 
   return (
