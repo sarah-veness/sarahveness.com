218915c75431a42c8a27eebab21803954c45d8cc
Sarah Veness <sarahhveness@gmail.com>
2023-08-11 14:38:13 -0700

    eslint went wild, but pass err when post cant be created for debugging

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,38 +4,38 @@ const router = express.Router();
 
 import Post from '../../models/Post.js';
 
-router.get('/test', (req, res) => res.send('post route testing'));
-
 router.get('/', (req, res) => {
   Post.find()
-    .then(posts => res.json(posts))
-    .catch(err => res.status(404).json({ nopostsfound: 'No Posts found' }));
-})
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(404).json({ nopostsfound: 'No Posts found' }));
+});
 
 router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
-    .then(post => res.json(post))
-    .catch(err => res.status(404).json({ nopostfound: 'No Post found' }));
-})
+    .then((post) => res.json(post))
+    .catch((err) => res.status(404).json({ nopostfound: 'No Post found' }));
+});
 
 router.post('/', (req, res) => {
   Post.create(req.body)
-    .then(post => res.json({ msg: 'Post added successfully' }))
-    .catch(err => res.status(400).json({ error: 'Unable to add this post' }));
-})
+    .then((post) => res.json({ msg: 'Post added successfully' }))
+    .catch((err) =>
+      res.status(400).json({ error: 'Unable to add this post', err })
+    );
+});
 
 router.put('/:id', (req, res) => {
   Post.findByIdAndUpdate(req.params.id, req.body)
-    .then(post => res.json({ msg: 'Updated successfully' }))
-    .catch(err =>
+    .then((post) => res.json({ msg: 'Updated successfully' }))
+    .catch((err) =>
       res.status(400).json({ error: 'Unable to update the Database' })
-    )
-})
+    );
+});
 
 router.delete('/:id', (req, res) => {
   Post.findByIdAndRemove(req.params.id, req.body)
-    .then(post => res.json({ mgs: 'Post entry deleted successfully' }))
-    .catch(err => res.status(404).json({ error: 'No such a post' }));
-})
+    .then((post) => res.json({ mgs: 'Post entry deleted successfully' }))
+    .catch((err) => res.status(404).json({ error: 'No such a post' }));
+});
 
 export default router;
