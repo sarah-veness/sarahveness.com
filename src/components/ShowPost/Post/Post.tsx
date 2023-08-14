7d1b6a568b3a13fc8117fbf49564aa0d0d7d91c5
Sarah Veness <sarahhveness@gmail.com>
2023-08-14 00:21:50 -0700

    render the html content

--- src/components/ShowPost/Post/Post.tsx
+++ src/components/ShowPost/Post/Post.tsx
@@ -6,12 +6,18 @@ import styles from './post.module.scss';
 
 const Post = ({ post }: PostProps) => {
   const publishedDate = formatDate(post.published_date);
+  const htmlContent = () => {
+    return { __html: post.content };
+  };
   return (
     <div className={styles.postContainer}>
       <p className={styles.tags}>{post.tags}</p>
       <h1 className={styles.title}>{post.title}</h1>
       <p className={styles.date}>{publishedDate}</p>
-      <div className={styles.content}>{post.content}</div>
+      <div
+        className={styles.content}
+        dangerouslySetInnerHTML={htmlContent()}
+      ></div>
     </div>
   );
 };
