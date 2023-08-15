d1ee56caebe524beb8ecc73f99514a855c393b66
Sarah Veness <sarahhveness@gmail.com>
2023-08-15 00:48:23 -0700

    change css to scss for index

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
