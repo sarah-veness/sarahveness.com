7816cad88735677fc787a3f11666845a1e2c1c5e
Sarah Veness <sarahhveness@gmail.com>
2023-08-15 00:48:48 -0700

    change squares to rectangles

--- src/components/Writing/Card/Card.tsx
+++ src/components/Writing/Card/Card.tsx
@@ -10,11 +10,12 @@ export default function Card(props: any) {
   return (
     <div className={styles.Card}>
       <Link to={`/writing/${post._id}`}>
-        <img src="https://placekitten.com/g/500/500" alt={post.title} />
+        <img src="https://placekitten.com/g/600/300" alt={post.title} />
         <div className={styles.Overlay}>
           <div className={styles.textContainer}>
             <h2 className={styles.title}>{post.title}</h2>
             <h3 className={styles.date}>{formattedDate}</h3>
+            <span>{post.tags}</span>
           </div>
         </div>
       </Link>
