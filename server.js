8e547b1a0937ffa74554fb59285985a655dff3e3
Sarah Veness <sarahhveness@gmail.com>
2023-08-15 20:24:56 -0700

    allow server to handle larger file size to support image upload

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import ViteExpress from 'vite-express';
-import connectDB from './config/db.js'
+import connectDB from './config/db.js';
 import cors from 'cors';
 
-import router from './routes/api/posts.js'
+import router from './routes/api/posts.js';
 
-const app = express()
+const app = express();
 
-connectDB()
+connectDB();
 
 app.use(cors({ origin: true, credentials: true }));
-app.use(express.json({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
-app.use('/api/posts', router)
+app.use('/api/posts', router);
 
-ViteExpress.listen(app, 3000, () => console.log('server listening on port 3000'))
+ViteExpress.listen(app, 3000, () =>
+  console.log('server listening on port 3000')
+);
