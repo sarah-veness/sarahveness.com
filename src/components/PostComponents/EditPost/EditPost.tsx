70f35d3045f5e3e06cb399bffcf3b537a29e8065
Sarah Veness <sarahhveness@gmail.com>
2023-08-16 00:46:48 -0700

    use PostForm component in EditPost

--- src/components/PostComponents/EditPost/EditPost.tsx
+++ src/components/PostComponents/EditPost/EditPost.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-import ContentEditor from '../ContentEditor/ContentEditor';
+import PostForm from '../PostForm/PostForm';
 
 import type { PostType } from '../../../types/Post';
 
-import { slugify, availableTags } from '../../../utilities';
+import { slugify } from '../../../utilities';
 
 export default function EditPost() {
   const [post, setPost] = useState<Partial<PostType>>({
@@ -23,15 +23,6 @@ export default function EditPost() {
   const navigate = useNavigate();
 
   const todaysDate = new Date().toLocaleDateString();
-
-  const handleTagChange = (tagName: string) => {
-    setPost((prevPost) => ({
-      ...prevPost,
-      tags: prevPost.tags!.includes(tagName)
-        ? prevPost.tags!.filter((name) => name !== tagName)
-        : [...prevPost.tags!, tagName],
-    }));
-  };
 
   useEffect(() => {
     axios
@@ -51,14 +42,6 @@ export default function EditPost() {
         console.log(`Error from EditPost, ${err}`);
       });
   }, [id, todaysDate]);
-
-  const onChange = (e: any) => {
-    setPost({ ...post, [e.target.name]: e.target.value });
-  };
-
-  const onContentChange = (value: string) => {
-    setPost({ ...post, content: value });
-  };
 
   const onSubmit = (e: any) => {
     e.preventDefault();
@@ -83,80 +66,11 @@ export default function EditPost() {
   };
 
   return (
-    <div className="EditPost">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 m-auto">
-            <br />
-            <Link to="/admin" className="btn btn-outline-warning float-left">
-              BACK TO POSTS
-            </Link>
-          </div>
-          <div className="col-md-8 m-auto">
-            <h1 className="display-4 text-center">Edit post</h1>
-            <p className="lead text-center">Update post's Info</p>
-          </div>
-        </div>
-
-        <div className="col-md-8 m-auto">
-          <form noValidate onSubmit={onSubmit}>
-            <div className="form-group">
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                placeholder="Title of the post"
-                name="title"
-                className="form-control"
-                value={post.title}
-                onChange={onChange}
-              />
-            </div>
-            <br />
-
-            <div className="form-group">
-              <label htmlFor="author">Author</label>
-              <input
-                type="text"
-                placeholder="Author"
-                name="author"
-                className="form-control"
-                value={post.author}
-                onChange={onChange}
-              />
-            </div>
-            <br />
-
-            <div className="form-group">
-              <ContentEditor
-                initialValue={post.content!}
-                handleOnChange={onContentChange}
-              />
-            </div>
-            <br />
-            <div className="form-group">
-              {availableTags.map((tag, i) => (
-                <label key={i}>
-                  <input
-                    type="checkbox"
-                    checked={post.tags!.includes(tag.name)}
-                    onChange={() => handleTagChange(tag.name)}
-                  />
-                  {tag.name.toUpperCase()}
-                </label>
-              ))}
-            </div>
-
-            <br />
-
-            <button
-              type="submit"
-              className="btn btn-outline-info btn-lg btn-block"
-            >
-              Update post
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <PostForm
+      post={post}
+      setPost={setPost}
+      onSubmit={onSubmit}
+      heading={'Edit Post'}
+    />
   );
 }
