a5583ec75437839e6872a45aedce145d1c843091
Sarah Veness <sarahhveness@gmail.com>
2023-08-15 23:45:45 -0700

    create ContentEditor component

--- src/components/PostComponents/ContentEditor/ContentEditor.tsx
+++ src/components/PostComponents/ContentEditor/ContentEditor.tsx
@@ -1,14 +1,51 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+import styles from './content-editor.module.scss';
+
 interface ContentEditorProps {
   initialValue: string;
   handleOnChange: (value: string) => void;
 }
 
 function ContentEditor({ initialValue, handleOnChange }: ContentEditorProps) {
+  const moduleOptions = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [
+        { list: 'ordered' },
+        { list: 'bullet' },
+        { indent: '-1' },
+        { indent: '+1' },
+      ],
+      ['link', 'image'],
+      ['clean'],
+    ],
+  };
+
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+  ];
   return (
-    <ReactQuill theme="snow" value={initialValue} onChange={handleOnChange} />
+    <ReactQuill
+      className={styles.quill}
+      theme="snow"
+      value={initialValue}
+      onChange={handleOnChange}
+      modules={moduleOptions}
+      formats={formats}
+    />
   );
 }
 
