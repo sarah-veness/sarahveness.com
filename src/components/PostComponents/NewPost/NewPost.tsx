5e2ace45a0ce9972ccf0160e75dd5a78cc04509f
Sarah Veness <sarahhveness@gmail.com>
2023-08-15 23:56:11 -0700

    clean up NewPost with new PostForm component

--- src/components/PostComponents/NewPost/NewPost.tsx
+++ src/components/PostComponents/NewPost/NewPost.tsx
@@ -1,44 +1,22 @@
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 
-import slugify from '../../../utilities/create-post-slug';
-import availableTags from '../../../utilities/tags';
+import PostForm from '../PostForm/PostForm';
 
 import type { PostType } from '../../../types/Post';
 
-import styles from './new-post.module.scss';
-
 const NewPost = () => {
-  const { user } = useAuth0();
-  const author = user?.name || 'Anonymous';
-
   const navigate = useNavigate();
   const [post, setPost] = useState<Partial<PostType>>({
     title: '',
     slug: '',
     content: '',
     tags: [],
-    author: author,
+    author: '',
+    updated_date: '',
     published_date: '',
   });
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPost({ ...post, [e.target.name]: e.target.value });
-  };
-
-  const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let slug = slugify(e.target.value);
-    setPost({ ...post, title: e.target.value, slug: slug });
-  };
-
-  const onContentChange = (value: string) => {
-    setPost({ ...post, content: value });
-  };
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -60,109 +38,13 @@ const NewPost = () => {
       });
   };
 
-  const handleTagChange = (tagName: string) => {
-    setPost((prevPost) => ({
-      ...prevPost,
-      tags: prevPost.tags!.includes(tagName)
-        ? prevPost.tags!.filter((name) => name !== tagName)
-        : [...prevPost.tags!, tagName],
-    }));
-  };
-
-  const moduleOptions = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [
-        { list: 'ordered' },
-        { list: 'bullet' },
-        { indent: '-1' },
-        { indent: '+1' },
-      ],
-      ['link', 'image'],
-      ['clean'],
-    ],
-  };
-
-  const formats = [
-    'header',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-  ];
-
   return (
-    <div className={styles.NewPostContainer}>
-      <h2 className={styles.addPost}>Add Post</h2>
-
-      <form noValidate onSubmit={onSubmit}>
-        <div className={styles.formGroup}>
-          <label htmlFor="title" className={styles.label}>
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            className={styles.formInput}
-            value={post.title}
-            onChange={onTitleChange}
-          />
-          <div className={styles.slug}>/{slugify(post.title!)}</div>
-        </div>
-        <div>
-          <span className={styles.label}>Author</span>
-          <span className={styles.author}>{author}</span>
-        </div>
-        <div className={styles.formGroup}>
-          <label className={styles.label}>Write</label>
-          <ReactQuill
-            className={styles.quill}
-            theme="snow"
-            value={post.content}
-            onChange={onContentChange}
-            modules={moduleOptions}
-            formats={formats}
-          />
-        </div>
-        <div className={styles.tagGroup}>
-          {availableTags.map((tag, i) => (
-            <label htmlFor={tag.name} key={i}>
-              <input
-                type="checkbox"
-                className={styles.checkbox}
-                checked={post.tags!.includes(tag.name)}
-                onChange={() => handleTagChange(tag.name)}
-                id={tag.name}
-              />
-              <div>
-                <span key={i} className={styles.tagLabel}>
-                  # {tag.name.toUpperCase()}
-                </span>
-              </div>
-            </label>
-          ))}
-        </div>
-        <div className={styles.formGroup}>
-          <input
-            type="date"
-            placeholder="published_date"
-            name="published_date"
-            value={post.published_date}
-            onChange={onChange}
-          />
-        </div>
-
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <PostForm
+      post={post}
+      setPost={setPost}
+      onSubmit={onSubmit}
+      heading={'New Post'}
+    />
   );
 };
 
